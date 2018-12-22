c39f4ca1eab839360dd19e6df36e14c4baa906e3
Fred <911651855@qq.com>
2018-12-22 17:56:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# openlayersJs
+
+> openlayersJs
+
+## 什么是openlayers？
+
+``` 
+openlayers是一个开源免费的js库，用于在网页中实现地图的动态显示和交互
+
+``` 
+## 官网
+
+```
+https://openlayers.org/
+
+```
+## 示意图
+
+```
+![示意图](./static/img/pic.png)
---
+++ src/components/map.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <div id="map"></div>
+  </div>
+</template>
+<script>
+import "ol/ol.css";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    const map = new Map({
+      target: "map",
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [-0.12755, 51.507222],
+        zoom: 5
+      })
+    });
+  }
+};
+</script>
+<style lang="stylus" scoped>
+.hello
+  width 100%
+  height 100%
+#map
+  width 100%
+  height 100%
+</style>
+
